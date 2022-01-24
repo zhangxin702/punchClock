4ac08c2bf4cbd18ddaeddc7df8a951526618a45b
Philip_117 <1633385010@qq.com>
2022-01-24 20:30:24 +0800

    The second huge merge, don't worried.

--- app.js
+++ app.js
@@ -5,13 +5,6 @@ App({
       // env: "zhangxinying-1g0quq2g2d68268c",
       env: wx.cloud.DYNAMIC_CURRENT_ENV,
     });
-
-    wx.login({
-      success: (res) => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-        this.globalData.userInfo = res.result.userInfo;
-      },
-    });
   },
 
   globalData: {
