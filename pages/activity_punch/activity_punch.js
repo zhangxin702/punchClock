eb34b2e578ff756fd135329e8e572fb217153f54
qqq <qqq>
2022-01-26 09:28:40 +0800

    merge update about activity_punch

--- pages/activity_punch/activity_punch.js
+++ pages/activity_punch/activity_punch.js
@@ -5,62 +5,43 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    tabs:[
+      {
+        tab_id:0,
+        name:"已参与活动",
+        isActive:true
+      },
+      {
+        tab_id:1,
+        name:"已组织活动",
+        isActive:false
+      }
+    ]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  //标题点击事件，从组件中传过来
+  handleTabsItemChange(e){
+    //对tabs进行一次生拷贝，以防影响到原来数据
+    //let index=JSON.parse(JSON.stringify(e.detail));
+    let{index}=e.detail;
+    let{tabs}=this.data;
+    //找哪个是index，是的改成isActive为true，否则为false
+    tabs.forEach((v,i) =>i===index?v.isActive=true:v.isActive=false);
+    this.setData({
+      tabs
+    })
+  },
+  //加载时获取page_id以此确保能够从按钮中走向正确样式
   onLoad: function (options) {
-
+    //如果没看见上面的组件可以把下面的注释划掉
+    //console.log(options);
+    const {page_id}=options;
+    let{tabs}=this.data;
+    //因为传过来的是string应该转为Number
+    const index=Number(page_id);
+    //找哪个是page_id，是的改成isActive为true，否则为false
+    tabs.forEach((v,i) =>i===index?v.isActive=true:v.isActive=false);
+    this.setData({
+      tabs
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
