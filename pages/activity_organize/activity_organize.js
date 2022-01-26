60848af5f42a43cef51aa27c0d665cab63594dc8
zhangxin702 <1344160239@qq.com>
2022-01-26 18:42:46 +0800

    change something in activity_organize
    change something in activity_organize

--- pages/activity_organize/activity_organize.js
+++ pages/activity_organize/activity_organize.js
@@ -1,85 +1,153 @@
-Page({
+import {
+  chooseImage,
+  uploadFile,
+  showToast,
+  actTableInsert,
+} from '../../async/index.js';
 
+Page({
   /**
    * 页面的初始数据
    */
   data: {
+    // 上传图片设置
+    images: [],
+    count: 1,
+    addedCount: 0,
+    tempFilePaths: [],
+  
+    // 活动简介
+    active_introduce: '',
+   
+    // 活动名字
+    active_name: '',
+    // 活动时间，具体到秒
+    startTime: '2019-03-15 10:45:45',
+    endTime: '2022-08-15 10:45:23',
+// 活动公告
+    active_announce:'',
+    //最低打卡次数
+    punch_num : 1
+  },
 
-   // 上传图片设置
-  images: [],
-  count: 1,
-  addedCount: 0,
-  // 活动时间
-  selectedActiveDate:'2022-1-23',
-  // 活动简介
-  active_introduce:"",
-  // 活动截止时间
-  activeDateEnd:"2022-2-23",
-  // 活动名字
-  active_name:""
+  // 打卡次数设置
+  num_blur: function (e) {
+    console.log(e.detail.value);
+    var that = this;
+    that.setData({
+      punch_num: e.detail.value,
+    });
+    // console.log(this.Date.active_announce)
+  },
+  
+  prevNum(){
+    this.setData({punch_num: this.data.punch_num + 1 });
+  },
+  nextNum(){
+    this.setData({punch_num: this.punch_num - 1 });
+  },
+
+
+// 打卡次数设置
+
+
+  // 改变时间
+  changeStartDate(e){
+    console.log(e.detail.value)
+    this.setData({ startTime: e.detail.value})
+  },
+  changeEndDate(e){
+    console.log(e.detail.value)
+    this.setData({ endTime: e.detail.value})
     
   },
+  async submit() {
+    if (this.data. endTime < this.data. startTime) {
+      await showToast({
+        title: '您选择的时间有误，请重新选择',
+      });
+      return;
+    }
+    if (this.data.active_name === '') {
+      await showToast({
+        title: '当前活动名称为空，请输入活动名称',
+      });
+      return;
+    }
+    if (this.data.active_introduce === '') {
+      await showToast({
+        title: '当前活动介绍为空，请输入活动介绍',
+      });
+      return;
+    }
+    if (this.data. active_announce === '') {
+      await showToast({
+        title: '当前活动公告为空，请输入活动介绍',
+      });
+      return;
+    }
 
-  
+    var res = await uploadFile({
+      tempFilePath: this.data.tempFilePaths[0],
+    });
+    var ree = await actTableInsert({
+      actTheme: this.data.active_name,
+      actContent: this.data.active_introduce,
+      nowTime: new Date(),
+      startTime: this.data.selectedActivexDate,
+      endTime: this.data.activeDateEnd,
+      actImg: res.fileID,
+    });
+
+  },
 
   // 上传图片有关函数
-  chooseImage() {
-    var that = this;
-    wx.chooseImage({
-      count: 1- that.data.addedCount,
-      sizeType: ['compressed'], // 可以指定是原图还是压缩图，默认二者都有
-      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-      success: function (res) {
-        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
-        that.setData({
-          images: that.data.images.concat(res.tempFilePaths),
-          addedCount: that.data.addedCount + res.tempFilePaths.length,
-        });
-      }
-    })
+  async chooseImage() {
+    var res = await chooseImage({
+      addedCount: this.data.addedCount,
+    });
+    console.log(res);
+    this.setData({
+      images: this.data.images.concat(res.tempFilePaths),
+      addedCount: this.data.addedCount + res.tempFilePaths.length,
+      tempFilePaths: res.tempFilePaths,
+    });
   },
+
   // 删除图片
-  deleteImage(e){
-    this.data.images.splice(e.detail,1)
+  deleteImage(e) {
+    this.data.images.splice(e.detail, 1);
     this.setData({
       images: this.data.images,
-      addedCount: this.data.addedCount - 1
-    })
+      addedCount: this.data.addedCount - 1,
+    });
   },
- // 活动时间填写
- dateChange:function(e){
-  let day = e.detail.value;
-  this.setData({
-    selectedActiveDate:day
-  });
-  // console.log(selectedActiveDate)
-},
-
-// 活动结束时间填写
-dateEnd:function(e){
-  let day = e.detail.value;
-  this.setData({
-    activeDateEnd:day
-  });
-  // console.log(selectedActiveDate)
-},
-
-// 活动简介设置  输入框失去焦点时,即触发事件
-bindTextAreaBlur: function(e) {
-  // console.log(e.detail.value);
-  var that = this;
-  that.setData({
-    active_introduce: e.detail.value
-  });
-},
-// 活动名字设置  输入框失去焦点时,即触发事件
-bindTextAreaBlurName: function(e) {
-  // console.log(e.detail.value);
-  var that = this;
-  that.setData({
-    active_name: e.detail.value
-  });
-},
 
 
-})
+
+  // 活动简介设置  输入框失去焦点时,即触发事件
+  bindTextAreaBlur: function (e) {
+    // console.log(e.detail.value);
+    var that = this;
+    that.setData({
+      active_introduce: e.detail.value,
+    });
+  },
+  // 活动名字设置  输入框失去焦点时,即触发事件
+  bindTextAreaBlurName: function (e) {
+    // console.log(e.detail.value);
+    var that = this;
+    that.setData({
+      active_name: e.detail.value,
+    });
+  },
+   // 活动公告设置  输入框失去焦点时,即触发事件
+   bindBlurAnnounce: function (e) {
+    console.log(e.detail.value);
+    var that = this;
+    that.setData({
+      active_announce: e.detail.value,
+    });
+    // console.log(this.Date.active_announce)
+  },
+});
