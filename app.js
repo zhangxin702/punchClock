aeff2b51d6b63ec8bcf0adfc1738d1fd0db8efea
ws <1062875689@qq.com>
2022-02-12 11:33:16 +0800

    获取openId函数已修复

--- app.js
+++ app.js
@@ -1,31 +1,22 @@
-import { getUserInfo } from "./async/async.js";
+import { getUserInfo, getOpenId } from './async/async.js';
 
 App({
   async onLaunch() {
     wx.cloud.init({
-      env: "zhangxinying-1g0quq2g2d68268c",
+      env: 'zhangxinying-1g0quq2g2d68268c',
       // env: wx.cloud.DYNAMIC_CURRENT_ENV,
     });
-    // let openId;
-    // wx.cloud // 先获取该用户的openId
-    //   .callFunction({
-    //     name: 'getOpenId',
-    //   })
-    //   .then((res) => {
-    //     console.log('openId: ', res.result);
-    //     console.log(res);
-    //     openId = res.result;
-    //   });
-    const openId = "user-12";
-
-    let userInfo = await wx.getStorageSync("userInfo"); // 先查本地缓存
+    const openId = await getOpenId();
+    // const openId = "user-12";
+    console.log(openId);
+    let userInfo = await wx.getStorageSync('userInfo'); // 先查本地缓存
     // 本地缓存查不到
     if (!userInfo) {
       userInfo = await getUserInfo(openId); // 再查云数据库
       if (!userInfo) {
         return;
       }
-      await wx.setStorageSync("userInfo", userInfo); // 写本地缓存
+      await wx.setStorageSync('userInfo', userInfo); // 写本地缓存
     }
     this.globalData.userInfo = userInfo;
   },
