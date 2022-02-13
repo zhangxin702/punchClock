309d50da1072114b7e8b06753efc73b2fb5c30c4
zhangxin702 <1344160239@qq.com>
2022-02-14 00:49:01 +0800

    change the picture path
    change the picture path

--- pages/classify/classify.js
+++ pages/classify/classify.js
@@ -6,14 +6,14 @@ Page({
   data: {
   
     classify: [
-      { id: "000", class: "iconfont icon-qita", name: "全部",bgImgUrl:"https://i.postimg.cc/MHTHqJ1d/8dfed916-2fd0-476d-9439-37b3c2a1cd51.png"  },
-      { id: "001", class: "iconfont icon-jianshen", name: "健身" ,bgImgUrl:"https://i.postimg.cc/W3bd3VTQ/image.png" },
-      { id: "002", class: "iconfont icon-tubiaozhizuomoban-", name: "阅读" ,bgImgUrl:"https://i.postimg.cc/zfdgTvtJ/image.png" },
-      { id: "003", class: "iconfont icon-kaoshi", name: "考试",bgImgUrl:"https://i.postimg.cc/d1qQqcGg/7cf4cad6-2fd8-411f-8fb6-a8f4913a1538.png"  },
+      { id: "000", class: "iconfont icon-qita", name: "全部",bgImgUrl:"https://7a68-zhangxinying-1g0quq2g2d68268c-1307395035.tcb.qcloud.la/projectImage/%E6%B4%BB%E5%8A%A8%E5%88%86%E7%B1%BB-%E5%85%A8%E9%83%A8.png"  },
+      { id: "001", class: "iconfont icon-jianshen", name: "健身" ,bgImgUrl:"https://7a68-zhangxinying-1g0quq2g2d68268c-1307395035.tcb.qcloud.la/projectImage/%E6%B4%BB%E5%8A%A8%E5%88%86%E7%B1%BB-%E5%81%A5%E8%BA%AB.png" },
+      { id: "002", class: "iconfont icon-tubiaozhizuomoban-", name: "阅读" ,bgImgUrl:"https://7a68-zhangxinying-1g0quq2g2d68268c-1307395035.tcb.qcloud.la/projectImage/%E6%B4%BB%E5%8A%A8%E5%88%86%E7%B1%BB-%E9%98%85%E8%AF%BB.png" },
+      { id: "003", class: "iconfont icon-kaoshi", name: "考试",bgImgUrl:"https://7a68-zhangxinying-1g0quq2g2d68268c-1307395035.tcb.qcloud.la/projectImage/%E6%B4%BB%E5%8A%A8%E5%88%86%E7%B1%BB-%E8%80%83%E8%AF%95.png"  },
       { id: "004", class: "iconfont icon-kaoyan", name: "考研",bgImgUrl:"https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fbpic.588ku.com%2Fart_origin_min_pic%2F19%2F06%2F18%2Ff060c6885e9d9cc776a81aa39e43d6f8.jpg"  },
-      { id: "005", class: "iconfont icon-yingyu1", name: "英语",bgImgUrl:"https://i.postimg.cc/fRgKNMXg/da40a191-18a0-459d-a487-394740385163.png"  },
-      { id: "006", class: "iconfont icon-qita", name: "考勤",bgImgUrl:"https://i.postimg.cc/7PmqDRsT/image.png"  },
-      { id: "007", class: "iconfont icon-qita", name: "其他",bgImgUrl:"https://i.postimg.cc/xjwV0SG8/c04f53a4-fcc3-4d72-b6af-f665b5cbce62.png"  },
+      { id: "005", class: "iconfont icon-yingyu1", name: "英语",bgImgUrl:"https://7a68-zhangxinying-1g0quq2g2d68268c-1307395035.tcb.qcloud.la/projectImage/%E6%B4%BB%E5%8A%A8%E5%88%86%E7%B1%BB-%E8%8B%B1%E8%AF%AD.png"  },
+      { id: "006", class: "iconfont icon-qita", name: "考勤",bgImgUrl:"https://7a68-zhangxinying-1g0quq2g2d68268c-1307395035.tcb.qcloud.la/projectImage/%E6%B4%BB%E5%8A%A8%E5%88%86%E7%B1%BB-%E8%80%83%E5%8B%A4.png"  },
+      { id: "007", class: "iconfont icon-qita", name: "其他",bgImgUrl:"https://7a68-zhangxinying-1g0quq2g2d68268c-1307395035.tcb.qcloud.la/projectImage/%E6%B4%BB%E5%8A%A8%E5%88%86%E7%B1%BB-%E5%85%B6%E4%BB%96.png"  },
     
     ]
   },
