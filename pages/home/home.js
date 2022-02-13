ea0ab6eb7ba0d3fada995df71fd1fa30b055b48e
zhangxin702 <1344160239@qq.com>
2022-02-13 17:18:04 +0800

    delete the calendar
    delete the calendar

--- pages/home/home.js
+++ pages/home/home.js
@@ -7,15 +7,7 @@ Page({
   data: {
    
 
-    // 日期
-    curYear: new Date().getFullYear(), // 年份
-    curMonth: new Date().getMonth(),// 月份 0-11
-    day: new Date().getDate(), // 日期 1-31 若日期超过该月天数，月份自动增加
-    header_show: true, // 主标题是否显示
-    prev: true, // 上一个月按钮是否显示
-    next: true, // 下一个月按钮是否显示
-    // ramark_show:true,
-    // count_txt:true
+    
 
     currentTab: 0,
     actList: [],
@@ -41,14 +33,6 @@ Page({
     wx.stopPullDownRefresh();
     console.log(this.data.actList);
   },
-  nextMonth: function (e) {
-    console.log(e)
-    const currentYear = e.detail.currentYear;
-    const currentMonth = e.detail.currentMonth;
-    wx.showModal({
-      title: '当前日期',
-      content: '当前年份：' + currentYear + '年\n当前月份：' + currentMonth + '月'
-    });
-  },
+
 
 });
