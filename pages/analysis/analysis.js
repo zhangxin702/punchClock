6ed8f3bb1a1832da7a2d36901f556aca09329ca5
qqq <qqq>
2022-02-20 23:27:59 +0800

    update with analysis and personal

--- pages/analysis/analysis.js
+++ pages/analysis/analysis.js
@@ -8,7 +8,8 @@ Page({
   },
 
   async onReady() {
-    const openId = app.globalData.userInfo._id,
+    // const openId = app.globalData.userInfo._id,
+    const openId = "user-1",
       actId = "1ef3c51361ed7d0c04d6b15c7b9b82b8";
     const db = wx.cloud.database();
     wx.showLoading({
@@ -22,13 +23,13 @@ Page({
     // const rank = await getSelfPunchedRank(openId, actId);
     // console.log("用户的打卡次数超过了: ", rank * 100, "%");
 
-    // const { maxLabels, labelNum } = await getSelfLabels(openId, -1);
-    // console.log("所有的标签：", maxLabels);
-    // this.setData({
-    //   //分成三份
-    //   labels: maxLabels, // 全部标签
-    //   maxLabels: maxLabels.slice(0, 3), // 标签的前三名
-    // });
+    const { maxLabels, labelNum } = await getSelfLabels(openId, -1);
+    console.log("所有的标签：", maxLabels);
+    this.setData({
+      //分成三份
+      labels: maxLabels, // 全部标签
+      maxLabels: maxLabels.slice(0, 3), // 标签的前三名
+    });
 
     // const { isFinish, punchedTimes } = await getActPunchedTimes(db, actId);
     // console.log("是否已完成打卡要求：\n", isFinish);
@@ -55,45 +56,75 @@ Page({
 
     // const {actThemes,actUserNum}=await GetUserPunch(openId);
     // console.log(actUserNum,actThemes);
-    chart.draw(this, "canvas1", {
-      title: {
-        text: "标签百分比图",
-        color: "#333333",
-      },
-      xAxis: {
-        data: this.data.labels,
-      },
-      series: [
-        {
-          name: this.data.labels,
-          category: "pie",
-          data: labelNum,
+    if(maxLabels.length&&labelNum.length)
+    {   
+      chart.draw(this, "canvas1", {
+        title: {
+          text: "用户活动参与标签百分比图",
+          color: "#333333",
         },
-      ],
-    });
+        xAxis: {
+          data: this.data.labels,
+        },
+        series: [
+          {
+            name: this.data.labels,
+            category: "pie",
+            data: labelNum,
+          },
+        ],
+      });
+    }
+    
+    if(actThemes.length&& userNum.length&& userRank.length){
+      chart.draw(this, "canvas2", {
+        title: {
+          text: "已组织打卡活动人数及其排名",
+          color: "#333333",
+        },
+        xAxis: {
+          data: actThemes.slice(0,6),
+        },
+        series: [
+          {
+            name: "用户数量",
+            category: "bar",
+            data: userNum.slice(0, 6),
+            // data:actUserNum.slice(0,6)
+          },
+          {
+            name: "用户数量总数排名",
+            category: "bar",
+            data: userRank.slice(0, 6),
+          },
+        ],
+      });
+    }
 
-    chart.draw(this, "canvas2", {
-      title: {
-        text: "已组织打卡活动情况",
-        color: "#333333",
-      },
-      xAxis: {
-        data: actThemes,
-      },
-      series: [
-        {
-          name: "用户数量排名",
-          category: "bar",
-          data: actUserRank.slice(0, 6),
-          // data:actUserNum.slice(0,6)
+    if(actThemes.length&& punchNum.length&& punchRank.length){
+      chart.draw(this, "canvas3", {
+        title: {
+          text: "已组织打卡活动打卡数及其排名",
+          color: "#333333",
         },
-        {
-          name: "打卡数量排名",
-          category: "line",
-          data: actPunchRank.slice(0, 6),
+        xAxis: {
+          data: actThemes.slice(0,6),
         },
-      ],
-    });
+        series: [
+          {
+            name: "用户数量",
+            category: "bar",
+            data: punchNum.slice(0, 6),
+            // data:actUserNum.slice(0,6)
+          },
+          {
+            name: "用户数量总数排名",
+            category: "bar",
+            data: punchRank.slice(0, 6),
+          },
+        ],
+      });
+    }
     wx.hideLoading();
   },
 });
